34b73856c0d552df005ce390142a18197aa7e0b2
billobah <billobah@yahoo.fr>
2019-12-25 21:33:09 +0100

    adding some changes

--- download-data.R
+++ download-data.R
@@ -1,3 +1,3 @@
 url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
-destination_file <- "data/murders.csv"
-download.file(url, destination_file)
+dest_file <- "data/murders.csv"
+download.file(url, destfile = dest_file)
